acd959517767dc64da880e21cbd2fe3d89967f20
Warma <noreply@github.com>
2021-11-19 13:56:18 +0000

    Automatic commit

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
 window.GeneralConfig = {
-    network: "rinkeby"
+    network: "rinkeby",
+
+    onlineChainId: '0x61',
+    chainName: ' Smart Chain - Testnet',
+    nativeCurrency: {
+        name: 'Binance Coin',
+        symbol: 'BNB',
+        decimals: 18
+    },
+    rpcUrls: ['https://bsc-dataseed.binance.org/'],
+    blockExplorerUrls: ['https://testnet.bscscan.com']
 };
