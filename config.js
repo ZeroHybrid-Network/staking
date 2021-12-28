f1d61039916e9de863c7a2fa9a1a7fac841394c9
Warma <noreply@github.com>
2021-12-28 07:07:00 +0000

    Automatic commit

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 window.GeneralConfig = {
-    network: "rinkeby",
+    network: "bsctest",
+
+    // token
+
+    // tokenAddress:'0xB89212Ca0b5e5223d2e37a23a3a3064878984528',
+    // tokenSymbol: 'ZHT',
+    // tokenDecimals: 18,
+    // tokenImage: 'http://zerohybrid.network/img/favicon.ico',
+
 
     onlineChainId: '0x61',
     chainName: ' Smart Chain - Testnet',
