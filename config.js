8bf0e0ef9e8a1c136864139dcdbc03c71ea044d8
Warma <noreply@github.com>
2021-12-29 17:30:00 +0000

    Automatic commit

--- config.js
+++ config.js
@@ -1,21 +1,24 @@
 window.GeneralConfig = {
-    network: "bsctest",
+    network: "bsc",
 
-    // token
+    // onlineChainId: '97',
+    // chainName: 'Binance Smart Chain Testnet',
+    // nativeCurrency: {
+    //     name: 'Binance Coin',
+    //     symbol: 'tBNB',
+    //     decimals: 18
+    // },
+    // rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+    // blockExplorerUrls: ['https://testnet.bscscan.com']
 
-    // tokenAddress:'0xB89212Ca0b5e5223d2e37a23a3a3064878984528',
-    // tokenSymbol: 'ZHT',
-    // tokenDecimals: 18,
-    // tokenImage: 'http://zerohybrid.network/img/favicon.ico',
 
-
-    onlineChainId: '0x61',
-    chainName: ' Smart Chain - Testnet',
+    onlineChainId: '56',
+    chainName: 'Binance Smart Chain Mainnet',
     nativeCurrency: {
         name: 'Binance Coin',
         symbol: 'BNB',
         decimals: 18
     },
-    rpcUrls: ['https://bsc-dataseed.binance.org/'],
-    blockExplorerUrls: ['https://testnet.bscscan.com']
+    rpcUrls: ['https://bsc-dataseed1.binance.org'],
+    blockExplorerUrls: ['https://bscscan.com']
 };
